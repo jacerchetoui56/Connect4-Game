f43043e02f2c0ad4aafbdbb632db29b4944c8fff
jacer <jacerdjo@gmail.com>
2023-01-17 17:34:24 +0100

    adding colors to the cmd

--- game.py
+++ game.py
@@ -1,12 +1,21 @@
 
 from board import Board
 from player import Player
+# these modules are used to make the input colored
+from colorama import init
+init()
+
+YELLOW = "\x1b[1;33;40m"
+RED = "\x1b[1;31;40m"
 
 
 class Game:
     def __init__(self):
-        playerOne = input("Player 1 name : ")
-        playerTwo = input("Player 2 name : ")
+        # making the input colored
+        print(f"\n{RED}Player 1 name : ", end='')
+        playerOne = input()
+        print(f"\n{YELLOW}Player 2 name : ", end='')
+        playerTwo = input()
         # TODO : add color choice
         self.player1 = Player(playerOne)
         self.player2 = Player(playerTwo)
